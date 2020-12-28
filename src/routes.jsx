4931fee76b22fcc48b9e8132631826284f425484
Devlin Duldulao <devlin.duldulao@inmeta.no>
2020-12-28 20:47:29 +0100

    updated

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,5 +1,5 @@
 import React, { lazy, Suspense } from "react";
-import { Redirect, Route, Switch } from "react-router";
+import { Redirect, Route, Switch } from "react-router-dom";
 import HomePage from "pages/HomePage";
 
 const Routes = () => (
